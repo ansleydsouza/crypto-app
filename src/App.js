d9fcd55d86b0894414798b26c6e0751558e2a131
Ansley <ansleydsouza@icloud.com>
2022-02-21 02:15:34 +0800

    Changed the layout
    Changed the display layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import './input.css'
 import Axios from 'axios';
 import { useState, useEffect } from 'react';
@@ -17,55 +16,40 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold">All Cryptocurrencies</h1>
-      <input
-        type = "text"
-        placeholder='Search'
-        onChange={(e) => {
-          setSearch(e.target.value);
-        }}
-        />
+    <div className="container mx-auto font-sans">
+      <h1 className="text-3xl font-bold text-cyan-600 text-center py-5">All Cryptocurrencies</h1>
+      <div className="items-center align-middle text-center py-5">
+        <input className="text-xl w-6/12 py-2 px-2"
+          type = "text"
+          placeholder='Search'
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          />
+      </div>
 
-        <table>
-          <thead>
-            <tr>
-              <td>Rank</td>
-              <td>Name</td>
-              <td>Symbol</td>
-              <td>Market Cap</td>
-              <td>Price</td>
-              <td>Available Supply</td>
-              <td>Volume(24hrs)</td>            
-            </tr>
-          </thead>
-          <tbody>
-          {crypto
+      <ul role="list" className="grid mt-6 grid-cols-4 gap-8">
+      {crypto
             .filter((val) => {
               return val.name.toLowerCase().includes(search.toLowerCase());
             })
             .map((val, id) => {
               return (
-                  <tr id={id}>
-                    <td className="rank">{val.rank}</td>
-                    <td className="logo">
+                  <li key={id} className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-300">
+                    <div className="flex-1 flex flex-col p-8">
                       <a href={val.websiteUrl}>
-                        <img src={val.icon} alt="logo" width="30px" />
-                      </a> 
-                      <p>{val.name}</p>
-                    </td>
-                    <td className="symbol">{val.symbol}</td>
-                    <td>${val.marketCap.toFixed(0)}</td>
-                    <td>${val.price.toFixed(2)}</td>
-                    <td>{val.availableSupply}</td>
-                    <td>{val.volume}</td>
-                  </tr>
+                        <img className="flex w-10 h-10 items-center" src={val.icon} alt="logo"/>
+                        <h3 className="flex mt-4 text-gray-900 text-lg font-medium">{val.name}</h3>
+                        <h4 className="flex">Symbol: {val.symbol}</h4>
+                        <h4 className="flex">Price: ${val.price.toFixed(2)}</h4>
+                        <h4 className="flex">Market Cap: {val.marketCap.toFixed(2)}</h4>
+                      </a>
+                    </div>
+                  </li>
               );
             })}
+      </ul>
 
-
-          </tbody>
-        </table>
     </div>
   );
 }
