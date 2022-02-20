121d09aa94e2379ee98650ddd8d3ea05f53f95a0
Ansley <ansleydsouza@icloud.com>
2022-02-20 17:32:47 +0800

    Initial Commit
    Added the initial wireframe for the application

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+Thanks to https://www.geeksforgeeks.org/how-to-create-a-cryptocurrency-app-in-reactjs/ for the tutorial.
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  //Set up the initial states
+  const [search, setSearch] = useState("");
+  const [crypto, setCrypto] = useState([]);
+
+  useEffect(() => {
+    Axios.get(`https://api.coinstats.app/public/v1/coins?skip=0&limit=100&currency=SGD`)
+    .then((res) => {
+      setCrypto(res.data.coins);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>All Cryptocurrencies</h1>
+      <input
+        type = "text"
+        placeholder='Search'
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+        />
+
+        <table>
+          <thead>
+            <tr>
+              <td>Rank</td>
+              <td>Name</td>
+              <td>Symbol</td>
+              <td>Market Cap</td>
+              <td>Price</td>
+              <td>Available Supply</td>
+              <td>Volume(24hrs)</td>            
+            </tr>
+          </thead>
+          <tbody>
+          {crypto
+            .filter((val) => {
+              return val.name.toLowerCase().includes(search.toLowerCase());
+            })
+            .map((val, id) => {
+              return (
+                  <tr id={id}>
+                    <td className="rank">{val.rank}</td>
+                    <td className="logo">
+                      <a href={val.websiteUrl}>
+                        <img src={val.icon} alt="logo" width="30px" />
+                      </a> 
+                      <p>{val.name}</p>
+                    </td>
+                    <td className="symbol">{val.symbol}</td>
+                    <td>${val.marketCap.toFixed(0)}</td>
+                    <td>${val.price.toFixed(2)}</td>
+                    <td>{val.availableSupply}</td>
+                    <td>{val.volume}</td>
+                  </tr>
+              );
+            })}
+
+
+          </tbody>
+        </table>
     </div>
   );
 }
